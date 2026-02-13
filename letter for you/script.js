ad1cc18af3d53a6d19ab23ee89ee9739e37ae515
chiaki <ilfirahmi0011@gmail.com>
2026-02-13 14:36:16 +0700

    Delete letter for you directory

--- letter for you/script.js
+++
@@ -1,8 +0,0 @@
-function goNext() {
-  const page = document.getElementById("page");
-  page.classList.add("fade-out");
-
-  setTimeout(() => {
-    window.location.href = "page2.html";
-  }, 600); // harus sama dengan durasi animasi
-}
